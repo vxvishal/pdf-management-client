6e57d466dad588df95f64b3403b6207baffae1e1
vxvishal <singhvishalkumar997@gmail.com>
2024-05-30 21:37:00 +0530

    added toasts, fixed vercel routing and form submissions

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,6 +10,7 @@ import {
     Heading,
     Link,
     useColorModeValue,
+    useToast
 } from '@chakra-ui/react';
 import { useLoginMutation } from "../../apis/user";
 import { useState } from 'react';
@@ -24,13 +25,24 @@ export default function Login() {
     const setToken = useStore((state: any) => state.setToken);
     const setData = useStore((state: any) => state.setData);
     const navigate = useNavigate();
+    const toast = useToast();
 
-    function handleLogin() {
+    function handleLogin(e: any) {
+        e.preventDefault();
         mutate({ email, password }, {
             onSuccess: (data) => {
                 setToken(data.token);
                 setData(jwtDecode(data.token));
                 navigate('/dashboard');
+            },
+            onError: (error: any) => {
+                toast({
+                    title: error.response.data.message,
+                    status: "error",
+                    duration: 2000,
+                    isClosable: true,
+                    position: "top-right"
+                });
             }
         });
     }
@@ -43,36 +55,39 @@ export default function Login() {
             bg={useColorModeValue('gray.50', 'gray.800')}>
             <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
-                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                    <Heading fontSize={'3rem'} color={'#4299e1'} pb={25}>SpotDraft</Heading>
+                    <Heading fontSize={'xl'}>Sign in to your account</Heading>
                 </Stack>
                 <Box
                     rounded={'lg'}
                     bg={useColorModeValue('white', 'gray.700')}
                     boxShadow={'lg'}
                     p={8}>
-                    <Stack spacing={4} onKeyDown={(e) => { if (email && password && e.key === "Enter") { handleLogin(); } }}>
-                        <FormControl id="email">
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" onChange={(e) => setEmail(e.target.value)} />
-                        </FormControl>
-                        <FormControl id="password">
-                            <FormLabel>Password</FormLabel>
-                            <Input type="password" onChange={(e) => setPassword(e.target.value)} />
-                        </FormControl>
-                        <Stack spacing={10}>
-                            <Button
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}
-                                onClick={handleLogin}
-                                isLoading={isLoading}
-                            >
-                                Sign in
-                            </Button>
+                    <form onSubmit={(e: any) => handleLogin(e)}>
+                        <Stack spacing={4} onKeyDown={(e) => { if (email && password && e.key === "Enter") { handleLogin(e); } }}>
+                            <FormControl id="email">
+                                <FormLabel>Email address</FormLabel>
+                                <Input autoFocus required type="email" onChange={(e) => setEmail(e.target.value)} />
+                            </FormControl>
+                            <FormControl id="password">
+                                <FormLabel>Password</FormLabel>
+                                <Input required type="password" onChange={(e) => setPassword(e.target.value)} />
+                            </FormControl>
+                            <Stack spacing={10}>
+                                <Button
+                                    type='submit'
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}
+                                    isLoading={isLoading}
+                                >
+                                    Sign in
+                                </Button>
+                            </Stack>
                         </Stack>
-                    </Stack>
+                    </form>
                     <Stack pt={6}>
                         <Text align={'center'}>
                             Don't have an account? <Link color={'blue.400'} onClick={() => navigate('/signup')}>Sign Up</Link>
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -10,6 +10,7 @@ import {
     Heading,
     Link,
     useColorModeValue,
+    useToast
 } from '@chakra-ui/react';
 import { useSingupMutation } from "../../apis/user";
 import { useState } from 'react';
@@ -24,15 +25,26 @@ export default function Signup() {
     const [password, setPassword] = useState("");
     const setToken = useStore((state: any) => state.setToken);
     const setData = useStore((state: any) => state.setData);
+    const toast = useToast();
 
     const navigate = useNavigate();
 
-    function handleSignup() {
+    function handleSignup(e: any) {
+        e.preventDefault();
         mutate({ name, email, password }, {
             onSuccess: (data) => {
                 setToken(data.token);
                 setData(jwtDecode(data.token));
                 navigate('/dashboard');
+            },
+            onError: (error: any) => {
+                toast({
+                    title: error.response.data.message,
+                    status: "error",
+                    duration: 2000,
+                    isClosable: true,
+                    position: "top-right"
+                });
             }
         });
     }
@@ -45,7 +57,8 @@ export default function Signup() {
             bg={useColorModeValue('gray.50', 'gray.800')}>
             <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
-                    <Heading fontSize={'4xl'}>Create an account</Heading>
+                    <Heading fontSize={'3rem'} color={'#4299e1'} pb={2}>SpotDraft</Heading>
+                    <Heading fontSize={'xl'}>Create an account</Heading>
                 </Stack>
                 <Box
                     rounded={'lg'}
@@ -53,33 +66,35 @@ export default function Signup() {
                     boxShadow={'lg'}
                     p={8}
                 >
-                    <Stack spacing={4} onKeyDown={(e) => { if (name && email && password && e.key === "Enter") { handleSignup(); } }}>
-                        <FormControl id="name">
-                            <FormLabel>Name</FormLabel>
-                            <Input type="text" onChange={(e) => setName(e.target.value)} />
-                        </FormControl>
-                        <FormControl id="email">
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" onChange={(e) => setEmail(e.target.value)} />
-                        </FormControl>
-                        <FormControl id="password">
-                            <FormLabel>Password</FormLabel>
-                            <Input type="password" onChange={(e) => setPassword(e.target.value)} />
-                        </FormControl>
-                        <Stack spacing={10}>
-                            <Button
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}
-                                onClick={handleSignup}
-                                isLoading={isLoading}
-                            >
-                                Sign up
-                            </Button>
+                    <form onSubmit={(e: any) => handleSignup(e)}>
+                        <Stack spacing={4} onKeyDown={(e) => { if (name && email && password && e.key === "Enter") { handleSignup(e); } }}>
+                            <FormControl id="name">
+                                <FormLabel>Name</FormLabel>
+                                <Input autoFocus required type="text" onChange={(e) => setName(e.target.value)} />
+                            </FormControl>
+                            <FormControl id="email">
+                                <FormLabel>Email address</FormLabel>
+                                <Input required type="email" onChange={(e) => setEmail(e.target.value)} />
+                            </FormControl>
+                            <FormControl id="password">
+                                <FormLabel>Password</FormLabel>
+                                <Input required type="password" onChange={(e) => setPassword(e.target.value)} />
+                            </FormControl>
+                            <Stack spacing={10}>
+                                <Button
+                                    type='submit'
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}
+                                    isLoading={isLoading}
+                                >
+                                    Sign up
+                                </Button>
+                            </Stack>
                         </Stack>
-                    </Stack>
+                    </form>
                     <Stack pt={6}>
                         <Text align={'center'}>
                             Already an user? <Link color={'blue.400'} onClick={() => navigate('/')}>Log In</Link>
